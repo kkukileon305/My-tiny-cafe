789b3352452b6016185fee2ba6b46f91a8a2dbfd
kkukileon305 <kkukileon305@gmail.com>
2022-07-25 16:37:01 +0900

    desc 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,7 @@ class MyDocument extends Document {
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700;900&display=swap' rel='stylesheet' />
+          <meta name='description' content='My tiny cafe' />
         </Head>
         <body>
           <Main />
